a16db01e6099e6db2fcbc4c71be7274fe59f9806
HakimM16 <hakimmabike2@gmail.com>
2025-03-08 19:17:50 +0000

    removed scripts

--- scripts/script_for_portfolio.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loaderContainer = document.querySelector(".loader-container");
-    
-    // Simulate loading
-    setTimeout(() => {
-        loaderContainer.style.display = "none"; // Hide loader
-        document.body.style.overflow = "auto"; // Enable scrolling
-    }, 1500); // Adjust to match animation-delay
-});
