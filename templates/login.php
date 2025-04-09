55f9ec58f2ea74c336f2746a869952041085b75b
HakimM16 <hakimmabike2@gmail.com>
2025-04-09 10:13:32 +0100

    Login.php is working

---
+++ templates/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // get data from the form
+        $email = $_POST['username'];
+        $password1 = $_POST['password'];
+
+        // connect to the database
+        $servername = "127.0.0.1";
+        $username = "root";
+        $password = "";
+        $dbname = "phase2";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // check if the username and password are correct
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $sql = "SELECT * FROM logininfo WHERE email='$email' AND password='$password1'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // redirect to addPost.php
+                header("Location: addPost.php");
+            } else {
+                echo "Invalid email or password";
+            }
+        }
+
+    ?>
+</body>
+</html>
