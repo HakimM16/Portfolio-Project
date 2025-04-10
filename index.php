a85d472786f87e3750e844bd901f8e63d2a3fa70
HakimM16 <hakimmabike2@gmail.com>
2025-04-10 13:09:04 +0100

    Changed the titles for each page

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Personal Portfolio Website HTM CSS Only</title>
+    <title>Home</title>
     <link rel="stylesheet" href="styles/home.css">
     <!-- home styling for mobile -->
     <link rel="stylesheet" href="styles/home_for_mobile.css">
