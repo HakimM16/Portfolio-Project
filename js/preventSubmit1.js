5b1caea4b3c8551ee35ecbecbae89957be3f8dcb
HakimM16 <hakimmabike2@gmail.com>
2025-04-14 13:48:14 +0100

    Added red highlighting when user doesn't input in addentry.php

--- js/preventSubmit1.js
+++ js/preventSubmit1.js
@@ -7,12 +7,32 @@ const preventSubmit = (event) => {
     if (input.value === "" && text.value === "") {
         event.preventDefault();
         alert("Please fill in both fields.");
+        input.style.border = "5px solid red";
+        text.style.border = "5px solid red";
+
+        setTimeout(function() {
+            console.log("This is shown after 3 seconds");
+            input.style.border = "none";
+            text.style.border = "none";
+        }, 1000);
     } else if (input.value === "") {
         event.preventDefault();
         alert("Please fill in the input field.");
+        input.style.border = "5px solid red";
+
+        setTimeout(function() {
+            console.log("This is shown after 3 seconds");
+            input.style.border = "none";
+        }, 1000);
     } else if (text.value === "") {
         event.preventDefault();
         alert("Please fill in the text field.");
+        text.style.border = "5px solid red";
+
+        setTimeout(function() {
+            console.log("This is shown after 3 seconds");
+            text.style.border = "none";
+        }, 1000);
     }
 }
 
