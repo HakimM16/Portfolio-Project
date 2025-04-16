4a61af1bef3d6ba465bfc60d6fd70b3976b4c0fe
HakimM16 <hakimmabike2@gmail.com>
2025-04-16 09:23:12 +0100

    Added pop-up to prevent a mistake when clicking the clear button

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,12 @@
-const clear = () => {
-    document.querySelector('input').value = '';
-    document.querySelector('textarea').value = '';
+
+const clear = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    if (confirm('Are you sure you want to clear the fields?')) {
+        document.querySelector('input').value = '';
+        document.querySelector('textarea').value = '';
+    } else {
+        alert('Fields not cleared!');
+    }
 }
 
 let clearButton = document.getElementById('clear');
